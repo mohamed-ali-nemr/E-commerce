9e6e7404f99afbb26af9ce88d9efb4e335256580
mohamed-ali-nemr <79578780+mohamed-ali-nemr@users.noreply.github.com>
2023-07-25 21:29:46 +0300

    Add files via upload
    edit 2 in posts with static data

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -7,16 +7,36 @@ const Posts = () => {
   let img2 = "https://images.unsplash.com/photo-1505740420928-5e560c06d30e";
   let img3 = "https://images.unsplash.com/photo-1454165205744-3b78555e5572";
   const [posts, setPost] = useState([
-    { id: 1, title: "post title 1", body: "post body ...", image: img1 },
-    { id: 2, title: "post title 2", body: "post body ...", image: img2 },
-    { id: 3, title: "post title 3", body: "post body ...", image: img3 },
+    {
+      id: 1,
+      title: "post title 1",
+      body: "post body ...",
+      image: img1,
+      author: "Admin1",
+    },
+    {
+      id: 2,
+      title: "post title 2",
+      body: "post body ...",
+      image: img2,
+      author: "Admin2",
+    },
+    {
+      id: 3,
+      title: "post title 3",
+      body: "post body ...",
+      image: img3,
+      author: "Admin3",
+    },
   ]);
+
+  const handleAddToCart = () => {};
+
   return (
     <section className="posts">
-       
-       {posts.map((post) => (
-         <Post key={post.id} />
-       ))}
+      {posts.map((post) => (
+        <Post key={post.id} post={post} handleAddToCart={handleAddToCart} />
+      ))}
 
       {/* <Post />
       <Post />
@@ -29,5 +49,3 @@ const Posts = () => {
 };
 
 export default Posts;
-
-
