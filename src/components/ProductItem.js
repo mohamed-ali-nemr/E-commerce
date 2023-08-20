ca419bf3a3e83f8408af8a693837580581145c48
mohamed ali <alinemrmohamed@gmail.com>
2023-08-20 17:50:46 +0300

    edit in products "home page "

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const ProductItem = ({ product, handleDetails }) => {
   return (
-    <Link to={`/details/${product.id}`} className="post">
+    <div  className="post">
       <img src={product.image} alt="" />
       <h3>{product.title}</h3>
       <p className="card-text">Price: {product.price}$</p>
-      <button className="btn" onClick={handleDetails}>
+      <Link to={`/details/${product.id}`} className="btn" onClick={handleDetails}>
         Details
-      </button>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
