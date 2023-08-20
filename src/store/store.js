5ddfc40fa284053127c80afd4054bb91264b1a43
mohamed ali <alinemrmohamed@gmail.com>
2023-08-20 17:29:01 +0300

    last edit

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,7 @@ import cartReducer from "./reducers";
 
 const initialState = {
   cart: [
-    {
-      product: {
-        id: 1,
-        title: "post title 1",
-        price: 1000,
-        body: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora fugiat quasi earum sapiente illum nulla? Deleniti id, nisi deserunt corporis et dolores ea veniam a quam quod, aliquid neque quidem amet aut nemo",
-        image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
-        author: "Admin1",
-      },
-      quantity:1
-    },
+    
     
   ],
 };
