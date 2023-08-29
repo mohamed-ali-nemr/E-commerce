a0db4c56872a9080522e1ae3907f4bd00c75b942
mohamed ali <alinemrmohamed@gmail.com>
2023-08-29 20:44:01 +0300

    checkout

--- src/pages/ChechOut.js
+++ src/pages/ChechOut.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {clearCart, setQuantity} from "../store/reducers/cart";
+import {clearCart} from "../store/reducers/cart";
 
 
 
